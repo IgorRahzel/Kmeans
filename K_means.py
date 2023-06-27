6cb613f43ee3687e569f000430c4df7a485e4d7c
Igor Rahzel <igorrahzel@gmail.com>
2023-06-27 11:04:26 -0300

    corrigindo pequenos erros

--- K_means.py
+++ K_means.py
@@ -4,6 +4,7 @@ from sklearn.metrics import silhouette_score, adjusted_rand_score
 
 start_time = time.time()
 
+# Imlementing function for the Minkowski distance
 def minkowski_distance(x1,x2,p):
     
     distance = np.abs(x1 - x2)
@@ -13,8 +14,9 @@ def minkowski_distance(x1,x2,p):
     
     return distance
 
+# Compute distance matrix based on the minkowski distance
 def distance_matrix(data,n,p):
-    col_limit = 1 #used to avoid redundant computations
+    col_limit = 1 # used to avoid redundant computations
     matrix = np.zeros((n,n))
     i = 0
     for i in range(n):
@@ -26,12 +28,13 @@ def distance_matrix(data,n,p):
                 col_limit+=1
     return matrix
         
-
+# 2-approximative kmeans algorithm
 def kmeans(k,num_points,data,p,dist_matrix):
-    if k > num_points:
-        return num_points #||mudar depois para retornar o conjunto de pontos||
+    if k >= num_points:
+        C = list(range(0,num_points))
+        C = np.array(C)
     else:
-        #choose initial random center
+        # choose initial random center
         center = np.random.choice(data.shape[0])
         C = np.array([center])
         while C.size < k:
@@ -43,13 +46,13 @@ def kmeans(k,num_points,data,p,dist_matrix):
                     distance_i_to_center.append(dist_matrix[c,i])
                 distance.append([np.max(distance_i_to_center)])
             new_center = np.argmax(distance)
-            #includes farthest point from the centers as center
+            # includes farthest point from the centers as center
             C = np.append(C,new_center)
     
-        return C
+    return C
 
+# Find the radius for a solution of the algorithm
 def solution_radius(C,data):
-
     radius = 0
     for i in range(data.shape[0]):
         dist_to_centers = []
@@ -60,6 +63,7 @@ def solution_radius(C,data):
     
     return radius
 
+# Indetify the label of each instance
 def labels(data,C):
     labels = np.zeros(data_matrix.shape[0])
     for i, point in enumerate(data_matrix):
@@ -69,26 +73,36 @@ def labels(data,C):
         labels[i] = np.argmin(distances)
     return labels
 
-#Create a ground truth label for the adjusted rand index
-def generate_labels(num_instances, num_centers,rng):
-    return rng.randint(low=0, high=num_centers, size=num_instances)
-
-
-csv_file = 'TCGA_InfoWithGrade.csv'
 # Load the CSV file into a NumPy array
+csv_file = 'TCGA_InfoWithGrade.csv'
 data_matrix = np.genfromtxt(csv_file, delimiter=',')
 data_matrix = np.delete(data_matrix,0,axis=0)
+
+# Get the number of instances
 num_rows = data_matrix.shape[0]
+
+#Extracting Ground Truth label
+ground_truth_labels = data_matrix[:,0].astype(int)
+data_matrix = np.delete(data_matrix,0, axis = 1)
+
+#distance matrix
 dist_matrix = distance_matrix(data_matrix,num_rows,2)
-C = kmeans(3,num_rows,data_matrix,2,dist_matrix)
-r = solution_radius(C,data_matrix)
-print('radius: ',r)
-pointslabels = labels(data_matrix,C)
-silhouette = silhouette_score(dist_matrix, pointslabels,metric='precomputed')
-print("Silhouette score:", silhouette)
-rng = np.random.RandomState(0)
-ground_truth_labels = generate_labels(num_rows,3,rng)
-ARI = adjusted_rand_score(ground_truth_labels, pointslabels)
-print("ARI:",ARI)
-print("--- %s seconds ---" % (time.time() - start_time))
+
+for i in range(30):
+    #Compute metrics
+    C = kmeans(3,num_rows,data_matrix,2,dist_matrix)
+    r = solution_radius(C,data_matrix)
+    pointslabels = labels(data_matrix,C)
+    silhouette = silhouette_score(dist_matrix, pointslabels,metric='precomputed')
+    ARI = adjusted_rand_score(ground_truth_labels, pointslabels)
+    runtime = time.time() - start_time
+    start_time = time.time()
+
+    #Print metrics
+    print("------------teste",i,"------------")
+    print('radius: ',r)
+    print("Silhouette score:", silhouette)
+    print("ARI:",ARI)
+    print("--- %s seconds ---" % runtime)
+    print('------------------------------------\n\n')
 
--- K_means_sklearn.py
+++ K_means_sklearn.py
@@ -1,6 +1,5 @@
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score, pairwise_distances,adjusted_rand_score
-from K_means import generate_labels
 import numpy as np
 import time
 
@@ -10,6 +9,10 @@ csv_file = 'TCGA_InfoWithGrade.csv'
 # Load the CSV file into a NumPy array
 data_matrix = np.genfromtxt(csv_file, delimiter=',')
 data_matrix = np.delete(data_matrix,0,axis=0)
+
+#Extracting Ground Truth label
+ground_truth_labels = data_matrix[:,0].astype(int)
+data_matrix = np.delete(data_matrix,0, axis = 1)
 
 # Create KMeans object with 2 clusters
 kmeans = KMeans(n_clusters=2,n_init='auto')
@@ -27,20 +30,19 @@ labels = kmeans.labels_
 #print("Labels:")
 #print(labels)
 
-# Compute the silhouette score
-silhouette_avg = silhouette_score(data_matrix, labels)
-print("Silhouette score:", silhouette_avg)
-
-#Compute ARI:
-rng = np.random.RandomState(0)
-ground_truth_labels = generate_labels(data_matrix.shape[0],3,rng)
-ARI = adjusted_rand_score(ground_truth_labels,labels)
-print("ARI:",ARI)
-
 # Compute the distances between each example and its centroid using Minkowski distance
 distances = pairwise_distances(data_matrix, centroids, metric='minkowski', p=2)
 
+
+# Compute the silhouette score
+silhouette_avg = silhouette_score(data_matrix, labels)
+# Compute Adjusted Rand Index
+ARI = adjusted_rand_score(ground_truth_labels,labels)
 # Compute the solution radius (maximum distance to centroid)
 radius = np.max(np.min(distances, axis=1))
+
 print("Solution radius:", radius)
+print("Silhouette score:", silhouette_avg)
+print("ARI:",ARI)
 print("--- %s seconds ---" % (time.time() - start_time))
+   
