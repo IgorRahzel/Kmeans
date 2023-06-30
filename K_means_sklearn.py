acf674766fa758a42e4974fcd977a84b4186d2a5
Igor Rahzel <igorrahzel@gmail.com>
2023-06-30 08:06:29 -0300

    salvando alteracoes

--- K_means_sklearn.py
+++ K_means_sklearn.py
@@ -2,20 +2,35 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score, pairwise_distances,adjusted_rand_score
 import numpy as np
 import time
+import argparse
 
 start_time = time.time()
 
-csv_file = 'spambase.data '
+parser = argparse.ArgumentParser(description='parameters for the kmeans algorithm')
+parser.add_argument('-f','--file',type = str,help='file containing dataset')
+parser.add_argument('-k','--num_clusters',type=int,help = 'number of clusters')
+parser.add_argument('-p','--minkowski_order',type = int,help = 'minkowski order')
+parser.add_argument('-c','--column',type = int, help = 'column of target atribute')
+parser.add_argument('-d','--delimiter',type = str,help = 'character used to separete data')
+parser.add_argument('-sh','--skip_header',type = int,help = 'skips headear from data')
+args = parser.parse_args()
+
+file = args.file
 # Load the CSV file into a NumPy array
-data_matrix = np.genfromtxt(csv_file, delimiter=',')
+if args.delimiter == ' ':
+    data_matrix = np.genfromtxt(file,skip_header=args.skip_header)
+else:
+    data_matrix = np.genfromtxt(file,delimiter=args.delimiter,skip_header=args.skip_header)
+
+
 data_matrix = np.delete(data_matrix,0,axis=0)
 
 #Extracting Ground Truth label
-ground_truth_labels = data_matrix[:,0].astype(int)
-data_matrix = np.delete(data_matrix,0, axis = 1)
+ground_truth_labels = data_matrix[:,args.column].astype(int)
+data_matrix = np.delete(data_matrix,args.column, axis = 1)
 
 # Create KMeans object with 2 clusters
-kmeans = KMeans(n_clusters=2,n_init='auto')
+kmeans = KMeans(n_clusters=args.num_clusters,n_init='auto')
 
 # Run K-Means algorithm
 kmeans.fit(data_matrix)
@@ -31,7 +46,7 @@ labels = kmeans.labels_
 #print(labels)
 
 # Compute the distances between each example and its centroid using Minkowski distance
-distances = pairwise_distances(data_matrix, centroids, metric='minkowski', p=2)
+distances = pairwise_distances(data_matrix, centroids, metric='minkowski', p=args.minkowski_order)
 
 
 # Compute the silhouette score
--- teste.py
+++ teste.py
@@ -1,107 +1,17 @@
 import numpy as np
-import time
-from sklearn.metrics import silhouette_score, adjusted_rand_score
 
-start_time = time.time()
+def contar_numeros_diferentes(matriz):
+    num_colunas = matriz.shape[1]
+    numeros_diferentes = []
 
-# Imlementing function for the Minkowski distance
-def minkowski_distance(x1,x2,p):
-    
-    distance = np.abs(x1 - x2)
-    distance = np.power(distance,p)
-    distance = np.sum(distance)
-    distance = distance**(1/p)
-    
-    return distance
+    for coluna in range(num_colunas):
+        numeros_coluna = np.unique(matriz[:, coluna])
+        numeros_diferentes.append(len(numeros_coluna))
 
-# Compute distance matrix based on the minkowski distance
-def distance_matrix(data,n,p):
-    col_limit = 1 # used to avoid redundant computations
-    matrix = np.zeros((n,n))
-    i = 0
-    for i in range(n):
-        for j in range(col_limit):
-            if j < i:
-                matrix[i,j] = minkowski_distance(data[i],data[j],p)
-                matrix[j,i] = matrix[i,j]
-            if i == j:
-                col_limit+=1
-    return matrix
-        
-# 2-approximative kmeans algorithm
-def kmeans(k,num_points,data,p,dist_matrix):
-    if k >= num_points:
-        C = list(range(0,num_points))
-        C = np.array(C)
-    else:
-        # choose initial random center
-        center = np.random.choice(data.shape[0])
-        C = np.array([center])
-        while C.size < k:
-            new_center = center
-            distance = []
-            for i in range(data.shape[0]):
-                distance_i_to_center = []
-                for c in C:
-                    distance_i_to_center.append(dist_matrix[c,i])
-                distance.append([np.max(distance_i_to_center)])
-            new_center = np.argmax(distance)
-            # includes farthest point from the centers as center
-            C = np.append(C,new_center)
-    
-    return C
+    return numeros_diferentes
 
-# Find the radius for a solution of the algorithm
-def solution_radius(C,data):
-    radius = 0
-    for i in range(data.shape[0]):
-        dist_to_centers = []
-        for center in C:
-            dist_to_centers.append(dist_matrix[center,i])
-        if(min(dist_to_centers) > radius):
-            radius = min(dist_to_centers)
-    
-    return radius
+# Exemplo de uso
+matriz = np.genfromtxt('winequality-white.csv',delimiter=';')
 
-# Indetify the label of each instance
-def labels(data,C):
-    labels = np.zeros(data_matrix.shape[0])
-    for i, point in enumerate(data_matrix):
-        distances = []
-        for center in C:
-            distances.append(minkowski_distance(point, data_matrix[center], 2))
-        labels[i] = np.argmin(distances)
-    return labels
-
-# Load the CSV file into a NumPy array
-csv_file = 'TCGA_InfoWithGrade.csv'
-data_matrix = np.genfromtxt(csv_file, delimiter=',')
-data_matrix = np.delete(data_matrix,0,axis=0)
-
-# Get the number of instances
-num_rows = data_matrix.shape[0]
-
-#Extracting Ground Truth label
-ground_truth_labels = data_matrix[:,0].astype(int)
-data_matrix = np.delete(data_matrix,0, axis = 1)
-
-#distance matrix
-dist_matrix = distance_matrix(data_matrix,num_rows,2)
-
-for i in range(30):
-    #Compute metrics
-    C = kmeans(2,num_rows,data_matrix,2,dist_matrix)
-    r = solution_radius(C,data_matrix)
-    pointslabels = labels(data_matrix,C)
-    silhouette = silhouette_score(dist_matrix, pointslabels,metric='precomputed')
-    ARI = adjusted_rand_score(ground_truth_labels, pointslabels)
-    runtime = time.time() - start_time
-    start_time = time.time()
-
-    #Print metrics
-    print("------------teste",i,"------------")
-    print('radius: ',r)
-    print("Silhouette score:", silhouette)
-    print("ARI:",ARI)
-    print("--- %s seconds ---" % runtime)
-    print('------------------------------------\n\n')
+resultado = contar_numeros_diferentes(matriz)
+print(resultado)
